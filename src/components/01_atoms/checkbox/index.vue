0640f3b19eefd4355ce6d6f1bcc57ffa9ff78691
IvanConor <pavlovic@vitamin2.ch>
2019-01-11 14:35:04 +0100

    added checkbox and option fields componetns

---
+++ src/components/01_atoms/checkbox/index.vue
@@ -0,0 +1,18 @@
+<!-- import html template -->
+<template src="./template.html"></template>
+
+<!-- start JS -->
+<script>
+/* eslint-disable */
+
+export default {
+  // name of the component
+  name: 'checkboxField',
+  props: [ 'list' ],
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
---
+++ src/components/01_atoms/radio/index.vue
@@ -0,0 +1,18 @@
+<!-- import html template -->
+<template src="./template.html"></template>
+
+<!-- start JS -->
+<script>
+/* eslint-disable */
+
+export default {
+  // name of the component
+  name: 'radioField',
+  props: [ 'list' ],
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
