52474ab162f940ddc8e192cd06018d51f5d0241b
IvanConor <pavlovic@vitamin2.ch>
2019-01-18 13:23:26 +0100

    changed mixins

---
+++ src/components/01_atoms/tooltip/index.vue
@@ -0,0 +1,17 @@
+<!-- import html template -->
+<template src="./template.html"></template>
+
+<!-- start JS -->
+<script>
+/* eslint-disable */
+
+export default {
+  // name of the component
+  name: 'Tooltip',
+  data() {
+    return {
+      tooltip: false
+    };
+  }
+};
+</script>
--- src/components/04_pages/LoginView.vue
+++ src/components/04_pages/LoginView.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="main-container">
-    <h1 class="page-title">Login</h1>
-    <p class="page-title-paragraph">Please log in or create a new account to play the game:</p>
+    <h1 class="page-title h1">Login</h1>
+    <p class="lead">Please log in or create a new account to play the game:</p>
     <form @keydown.enter="login">
       <InputField v-model="mail" typeProp="email" labelName="E-Mail" />
-      <InputField v-model="password" typeProp="password" labelName="Password" />
+      <InputField v-model="password" typeProp="password"
+      labelName="Password" />
     </form>
     <br>
     <Btn text="Login" primary="true" @click.native="login" iconName="arrow-right" />
