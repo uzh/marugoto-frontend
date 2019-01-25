29d18adde06c0a226dd86216452f4939653a280a
IvanConor <pavlovic@vitamin2.ch>
2019-01-25 14:55:14 +0100

    added topic selection functionality

---
+++ src/components/02_molecules/pageComponents/topic/index.vue
@@ -0,0 +1,8 @@
+<template src="./template.html"></template>
+
+<script>
+export default {
+  name: 'TopicComponent',
+  props: [ 'list' ],
+}
+</script>
--- src/components/04_pages/StorylineView.vue
+++ src/components/04_pages/StorylineView.vue
@@ -2,9 +2,7 @@
   <div class="storyline-container">
     <!-- <Btn @click.native="logout" text="Logout" primary="true" iconName="arrow-right"/> -->
     <div v-if="!get_topic.selected">
-      <div v-for="(topic, index) in get_topic.list" :key="index">
-        <p @click="chooseTopic(topic.id)">{{ topic.title }}</p>
-      </div>
+      <TopicComponent :list="get_topic.list" @openTopic="chooseTopic" />
     </div>
     <div v-if="get_topic.selected">
       <h1 class="storyline-title">{{ get_page.title }}</h1>
@@ -25,9 +23,11 @@ import Btn from '@/components/01_atoms/buttons'
 import PageTransitions from '@/components/02_molecules/pageTransitions'
 import PageComponents from '@/components/02_molecules/pageComponents'
 
+import TopicComponent from '@/components/02_molecules/pageComponents/topic'
+
 export default {
   name: 'player',
-  components: { Btn, PageTransitions, PageComponents },
+  components: { Btn, PageTransitions, PageComponents, TopicComponent },
   computed: {
     ...mapGetters([ 'get_page', 'get_topic' ]),
   },
@@ -45,6 +45,11 @@ export default {
   },
   methods: {
     ...mapActions(['LOGOUT']),
+
+    alertTopic: function(id) {
+      alert(id);
+    },
+
     chooseTopic: function(id){
       this.$store.dispatch('CHOOSE_TOPIC', id)
       .then(() => {
