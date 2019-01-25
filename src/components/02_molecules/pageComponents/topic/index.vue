abb630c695c007da806fc894f4beabc2743f8cae
IvanConor <pavlovic@vitamin2.ch>
2019-01-25 17:05:58 +0100

    modified topic elements

--- src/components/02_molecules/pageComponents/topic/index.vue
+++ src/components/02_molecules/pageComponents/topic/index.vue
@@ -1,8 +1,11 @@
 <template src="./template.html"></template>
 
 <script>
+import Btn from '@/components/01_atoms/buttons';
+
 export default {
   name: 'TopicComponent',
+  components: { Btn },
   props: [ 'list' ],
 }
 </script>
