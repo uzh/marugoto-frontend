e989a09fc5b02a70f43db21a529fa33700834781
IvanConor <pavlovic@vitamin2.ch>
2019-02-04 16:01:04 +0100

    modified notebook entries

--- src/components/03_organisms/notebookContainer/notebookPages/index.vue
+++ src/components/03_organisms/notebookContainer/notebookPages/index.vue
@@ -2,10 +2,11 @@
 
 <script>
 import svgIcon from '@/components/01_atoms/svgicon';
+import Btn from '@/components/01_atoms/buttons';
 
 export default {
   name: 'NotebookPages',
-  components: { svgIcon },
+  components: { svgIcon, Btn },
   props: [ 'currentEntry', 'list', 'direction' ],
   data(){
     return {
