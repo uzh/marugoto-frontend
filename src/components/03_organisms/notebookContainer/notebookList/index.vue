62c9c82ae59770bcb5ba304e727d65a274f87dc0
IvanConor <pavlovic@vitamin2.ch>
2019-02-25 14:31:50 +0100

    changed mail body links color, fixed markdowns for notebook lists and notebook header

--- src/components/03_organisms/notebookContainer/notebookList/index.vue
+++ src/components/03_organisms/notebookContainer/notebookList/index.vue
@@ -1,9 +1,11 @@
 <template src="./template.html"></template>
 
 <script>
+import VueMarkdown from 'vue-markdown';
 
 export default {
   name: 'NotebookList',
+  components: { VueMarkdown },
   props: [ 'list', 'selected' ],
   data(){
     return{
