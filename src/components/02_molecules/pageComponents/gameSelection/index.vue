d244521ecbae195cc566104e71411edb152a5a04
IvanConor <pavlovic@vitamin2.ch>
2019-03-14 11:45:47 +0100

    modified download list for game selection

--- src/components/02_molecules/pageComponents/gameSelection/index.vue
+++ src/components/02_molecules/pageComponents/gameSelection/index.vue
@@ -11,28 +11,42 @@ export default {
     return {
       classGames: [
         {
-          title: 'Plantation lives'
+          title: 'Plantation lives',
+          id: 'g4298ruvwjv9'
         },
       ],
       openGames: [
         {
-          title: 'Plantation lives'
+          title: 'Plantation lives',
+          id: 'f30jevr3j39jvr'
         },
         {
-          title: 'The Topic deep down in the sea'
+          title: 'The Topic deep down in the sea',
+          id: '9813ry8427tgf7e'
         },
         {
-          title: 'Another Topic'
+          title: 'Another Topic',
+          id: 'asxmqospqef0wf02j023'
         },
       ],
       finishedGames: [
         {
-          title: 'Japanese sailsmen'
+          title: 'Japanese sailsmen',
+          id: '0219y39f7y817efgiehfc'
         },
         {
-          title: 'Hunger Games'
+          title: 'Hunger Games',
+          id: '20183hfhf239h2942g'
         },
       ]
+    }
+  },
+  methods: {
+    continueGame: function(id) {
+      alert(`Game with id: ${id} continued`);
+    },
+    downloadGame: function(id) {
+      alert(`PDF of game with id: ${id} downloaded`);
     }
   }
 }
