772d540d5c9390d7ed6a6adfd35997f59ab589ae
IvanConor <pavlovic@vitamin2.ch>
2019-03-26 10:42:41 +0100

    modified styles after review

--- src/components/04_pages/StorylineView.vue
+++ src/components/04_pages/StorylineView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main-container">
-    <div class="storyline-container col-xs-12" v-if="get_topic.selected">
+    <div class="storyline-container" v-if="get_topic.selected">
       <div class="storyline-title col-xs-12">
         <div class="page-title-icon-container col-xs-2"> 
           <img v-if="get_page.chapter.icon && get_page.chapter.icon.path" class="icon" :src="`${resourcesPath}${get_page.chapter.icon.path}`">
