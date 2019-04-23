2e7ed6acf37af3f641d3f5095577c3e3e600f3a5
zlaxi <vujicic@vitamin2.ch>
2019-04-23 15:10:47 +0200

    Removed markdown content

--- src/components/02_molecules/pageComponents/index.vue
+++ src/components/02_molecules/pageComponents/index.vue
@@ -50,21 +50,21 @@ export default {
     };
   },
   mounted() {
-    this.controllComponentLayout();
-    if(document.getElementById("video")) {
-      let self = this;
-      document.getElementById("video").onloadedmetadata = function() {
-        let minutes = Math.floor(document.getElementById("video").duration / 60);
-        let seconds = Math.floor(document.getElementById("video").duration) - minutes * 60;
-        if (seconds % 60 < 10) {
-          self.videoDuration = `${minutes}:0${seconds}`;
-        } else {
-          self.videoDuration = `${minutes}:${seconds}`;
-        }
-      };
-      this.videoID = document.getElementById("video");
-      this.videoSeekBar = document.getElementById("video-seek-bar");
-    }
+    // this.controllComponentLayout();
+    // if(document.getElementById("video")) {
+    //   let self = this;
+    //   document.getElementById("video").onloadedmetadata = function() {
+    //     let minutes = Math.floor(document.getElementById("video").duration / 60);
+    //     let seconds = Math.floor(document.getElementById("video").duration) - minutes * 60;
+    //     if (seconds % 60 < 10) {
+    //       self.videoDuration = `${minutes}:0${seconds}`;
+    //     } else {
+    //       self.videoDuration = `${minutes}:${seconds}`;
+    //     }
+    //   };
+    //   this.videoID = document.getElementById("video");
+    //   this.videoSeekBar = document.getElementById("video-seek-bar");
+    // }
     if(document.getElementById("audio")) {
       let self = this;
       document.getElementById("audio").onloadedmetadata = function() {
