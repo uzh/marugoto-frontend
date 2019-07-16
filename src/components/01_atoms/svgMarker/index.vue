dfa2464e0bdc84ea4309589a1845b502a17b7f4f
zlaxi <vujicic@vitamin2.ch>
2019-07-16 07:36:12 +0200

    Datepicker width aadjustment

--- src/components/01_atoms/svgMarker/index.vue
+++ src/components/01_atoms/svgMarker/index.vue
@@ -21,9 +21,11 @@ export default {
       var start = `M32,-10`;
       var topLine = `C${refW - 70},-12 ${refW - 80},-11 ${refW},-13`;
       var topRightCurve = `C${refW},-12 ${refW + 50},-13 ${refW + 21},${refH}`;
+      var rightLine = `C${refW},-12 ${refW + 50},-13 ${refW + 21},${refH}`;
       var bottomRightCurve = `C${refW - 56},${refH + 14} ${refW + 50},${refH + 1} ${refW - 62},${refH + 10}`;
       var bottomLine = `C${0 + 56},${refH + 25} ${0 + 161},${refH + 10} ${20},${refH + 10}`;
       var bottomLeftCurve = `C${-3},${25} ${-5},${1} ${0},${0}`;
+      var leftLine = `C${refW},-12 ${refW + 50},-13 ${refW + 21},${refH}`;
       var endLine = `C${35},${-6} ${35},${-5} ${170},${-5}`;
 
       this.$refs[`path-${this.order}`].setAttribute("d", `${start}
