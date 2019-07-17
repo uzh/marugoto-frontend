79bf56c01c9f7f017b4631417bbd9971c8ba2f16
zlaxi <vujicic@vitamin2.ch>
2019-07-17 11:17:52 +0200

    Notebook nav scg fix

--- src/components/01_atoms/svgMarker/index.vue
+++ src/components/01_atoms/svgMarker/index.vue
@@ -10,35 +10,31 @@ export default {
   },
   methods: {
     drawSVG: function() {
-      // var svgParent = this.$refs[`svg-${this.order}`].parentElement.parentElement;
-      // var referenceElement = svgParent.getElementsByClassName('title')[0].children[0];
-      // console.log(referenceElement.offsetWidth, referenceElement.offsetHeight)
-      // var refW = referenceElement.offsetWidth;
-      // var refH = referenceElement.offsetHeight;
+      var svgParent = this.$refs[`svg-${this.order}`].parentElement.parentElement;
+      var referenceElement = svgParent.getElementsByClassName('title')[0].children[0];
       
-      // this.$refs[`svg-${this.order}`].style.width = refW + 'px';
-      // this.$refs[`svg-${this.order}`].style.height = refH + 'px';
-
-      // let negativeOffset = -17;
-      // let offsetWidth = refH / 2;
+      var refW = referenceElement.offsetWidth;
+      var refH = referenceElement.offsetHeight;
       
-      // var start = `M32,${negativeOffset}`;
-      // var topLine = `C${refW / 2},${negativeOffset} ${(refW - 15) - 10},-18 ${refW},0`;
-      // var topRightCurve = `C${refW + 10},${(refH / 2) / 2} ${refW + 10},${(refH / 2) / 2} ${refW + 10},${refH / 2}`;
-      // var bottomRightCurve = `C${refW - 56},${refH + 14} ${refW + 50},${refH + 1} ${refW - 62},${refH + 10}`;
-
-      // var rightLine = `C${refW + 20 + (refH / 2)},${refH / 2 - 20} ${refW + 20 + (refH / 2)},${refH / 2} ${refW + 20 + (refH / 2)},${refH / 2}`;
+      this.$refs[`svg-${this.order}`].style.width = refW + 'px';
+      this.$refs[`svg-${this.order}`].style.height = refH + 'px';
       
+      var start = `M32,-17`;
+      var topLine = `C${refW / 2},-17 ${(refW - 15) - 10},-18 ${refW},0`;
+      var topRightCurve = `C${refW + 10},${(refH / 2) / 2} ${refW + 10},${(refH / 2) / 2} ${refW + 10},${refH / 2}`;
+      var bottomRightCurve = `C${refW + 10},${refH / 2 + 10} ${refW + 10},${refH} ${refW},${refH + 10}`;
+      var bottomLine = `C${refW},${refH + 20} ${refW / 3},${refH + 30} 10,${refH + 10}`;
+      var bottomLeftCurve = `C-10,${refH} -10,${refH / 2}  -10,${refH / 2}`;
+      var endLine = `C-10,-5 0,-5 ${refW / 2},-5`;
       
-      // var bottomLine = `C${0 + 56},${refH + 25} ${0 + 161},${refH + 10} ${20},${refH + 10}`;
-      // var bottomLeftCurve = `C${-3},${25} ${-5},${1} ${0},${0}`;
-      // var endLine = `C${35},${-6} ${35},${-5} ${170},${-5}`;
-
-      // this.$refs[`path-${this.order}`].setAttribute("d", `${start}
-      //                                                     ${topLine}
-      //                                                     ${topRightCurve}
-      //                                                     ${bottomRightCurve}
-      //                                                     `);
+      this.$refs[`path-${this.order}`].setAttribute("d", `${start}
+                                                          ${topLine}
+                                                          ${topRightCurve}
+                                                          ${bottomRightCurve}
+                                                          ${bottomLine}
+                                                          ${bottomLeftCurve}
+                                                          ${endLine}
+                                                          `);
     },
   },
 }
